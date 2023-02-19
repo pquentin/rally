2e8e96e7b8b3e73b635c385654f1754024b9a5fe
Quentin Pradet <quentin.pradet@elastic.co>
2023-02-19 20:37:17 +0400

    Try running 3.12-dev in GitHub Actions + nox

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12-dev"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def test(session: nox.Session) -> None:
     session.install(".[develop]")
     session.run("pytest")
