c031c70fa358c4c5ef12e6653d49baa540991ad7
Quentin Pradet <quentin.pradet@elastic.co>
2023-02-19 07:36:56 +0400

    Support Python 3.11 and 3.12-dev

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12-dev"])
 def test(session: nox.Session) -> None:
     session.install(".[develop]")
     session.run("pytest")
